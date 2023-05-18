f1d69f9b891f6dee6b0e8d34ce6bb2c4016ff961
heidegomes <heidegc@hotmail.com>
2023-05-17 21:06:05 -0300

    ajuste de lint!

--- src/middlewares/validateAge.js
+++ src/middlewares/validateAge.js
@@ -3,25 +3,25 @@ module.exports = (req, res, next) => {
 
   if (age === undefined) {
     return res.status(400).json(
-      { message: "O campo \"age\" é obrigatório" },
+      { message: 'O campo \"age\" é obrigatório"' },
     );
   }
 
   if (typeof age !== "number") {
     return res.status(400).json(
-      { message: "O campo \"age\" deve ser um número inteiro igual ou maior que 18" },
+      { message: 'O campo \"age\" deve ser um número inteiro igual ou maior que 18' },
     );
   }
 
   if (age % 1 !== 0) {
     return res.status(400).json(
-      { message: "O campo \"age\" deve ser um número inteiro igual ou maior que 18" },
+      { message: 'O campo \"age\" deve ser um número inteiro igual ou maior que 18' },
     );
   }
 
   if (age < 18) {
     return res.status(400).json(
-      { message: "O campo \"age\" deve ser um número inteiro igual ou maior que 18" },
+      { message: 'O campo \"age\" deve ser um número inteiro igual ou maior que 18' },
     );
   }
   next();
--- src/middlewares/validateRate.js
+++ src/middlewares/validateRate.js
@@ -3,25 +3,25 @@ module.exports = (req, res, next) => {
   const { rate } = req.body.talk;
   if (rate === undefined) {
     return res.status(400).json(
-      { message: "O campo \"rate\" é obrigatório" },
+      { message: 'O campo \"rate\" é obrigatório' },
     );
   }
 
   if (rate % 1 !== 0) {
     return res.status(400).json(
-      { message: "O campo \"rate\" deve ser um número inteiro entre 1 e 5" },
+      { message: 'O campo \"rate\" deve ser um número inteiro entre 1 e 5' },
     );
   }
 
   if (rate <= 0) {
     return res.status(400).json(
-      { message: "O campo \"rate\" deve ser um número inteiro entre 1 e 5" },
+      { message: 'O campo \"rate\" deve ser um número inteiro entre 1 e 5' },
     );
   }
 
   if (rate > 6) {
     return res.status(400).json(
-      { message: "O campo \"rate\" deve ser um número inteiro entre 1 e 5" },
+      { message: 'O campo \"rate\" deve ser um número inteiro entre 1 e 5' },
     );
   }
   next();
