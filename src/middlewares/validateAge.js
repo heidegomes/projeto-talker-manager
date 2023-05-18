14a0d44f8c6bcaebe21bf1cdccc2b8c7bd387189
heidegomes <heidegc@hotmail.com>
2023-05-17 22:28:20 -0300

    ajuste de lint!

--- src/middlewares/validateAge.js
+++ src/middlewares/validateAge.js
@@ -7,44 +7,11 @@ module.exports = (req, res, next) => {
     );
   }
 
-  if (typeof age !== 'number') {
+  if (typeof age !== 'number' || age % 1 !== 0 || age < 18) {
     return res.status(400).json(
       { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
     );
   }
-
-  if (age % 1 !== 0) {
-    return res.status(400).json(
-      { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
-    );
-  }
-
-  if (age < 18) {
-    return res.status(400).json(
-      { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
-    );
-  }
-
-  // switch (age) {
-  //   case age === undefined:
-  //     return res.status(400).json(
-  //       { message: 'O campo "age" é obrigatório' },
-  //     );
-  //   case age !== 'number':
-  //     return res.status(400).json(
-  //       { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
-  //     );
-  //   case age % 1 !== 0:
-  //     return res.status(400).json(
-  //       { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
-  //     );
-  //   case age < 18:
-  //     return res.status(400).json(
-  //       { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
-  //     );
-  //   default:
-  //     break;
-  // }
 
   next();
 };
